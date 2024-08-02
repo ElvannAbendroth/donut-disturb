7962af01ec797d4a4d83caa07a25842e718328b2
Elvann <elvann.abendroth@gmail.com>
2024-08-02 14:17:56 +0300

    ⚙️fix deploy

--- src/content/config.ts
+++ src/content/config.ts
@@ -53,70 +53,11 @@ const featureCollection = defineCollection({
     }),
 })
 
-const teamCollection = defineCollection({
-  type: 'data',
-  schema: ({ image }) =>
-    z.object({
-      name: z.string().max(72),
-      role: z.string().max(72),
-      bio: z.string().optional(),
-      image: z.object({
-        src: image(),
-        alt: z.string(),
-      }),
-    }),
-})
-
-const blockCollection = defineCollection({
-  type: 'content',
-  schema: ({ image }) =>
-    z.object({
-      title: z.string().optional(),
-      image: z
-        .object({
-          src: image(),
-          alt: z.string(),
-        })
-        .optional(),
-      primaryCta: z
-        .object({
-          label: z.string(),
-          icon: z.string(),
-          href: z.string(),
-        })
-        .optional(),
-      secondaryCta: z
-        .object({
-          label: z.string(),
-          icon: z.string(),
-          href: z.string(),
-        })
-        .optional(),
-    }),
-})
-
-const testimonialCollection = defineCollection({
-  type: 'content',
-  schema: ({ image }) =>
-    z.object({
-      draft: z.boolean(),
-      message: z.string(),
-      author: z.string(),
-      image: z.object({
-        src: image(),
-        alt: z.string(),
-      }),
-    }),
-})
-
 // 3. Export a single `collections` object to register your collection(s)
 //    This key should match your collection directory name in "src/content"
 export const collections = {
   blog: blogCollection,
   page: pageCollection,
-  team: teamCollection,
   project: projectCollection,
-  block: blockCollection,
   feature: featureCollection,
-  testimonial: testimonialCollection,
 }
